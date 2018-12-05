4c3c92a0df96f833920c840aa72c38d194738437
dorhen <44751515+dorhen@users.noreply.github.com>
2018-12-05 17:25:28 +0200

    Update 18.4 The 3-sum problem.java

--- Elements of Programming Interviews - Java/18.4 The 3-sum problem.java
+++ Elements of Programming Interviews - Java/18.4 The 3-sum problem.java
@@ -1,12 +1,18 @@
+
 import java.util.*;
 class Triplet{
+    public static void main(String[] args){
+        int[] arr = {-5,2};
+        System.out.println(tripletSum2(arr,-7));
+    }
+    //O(nlogn) runtime, O(n) memory. (Assuming Arrays.sort has O(nlogn) runtime complexity)
     static boolean tripletSum(int[] arr, int sum){
         Arrays.sort(arr);
         HashSet<Integer> lowerSum = new HashSet<Integer>();
         for(int i = 0; i < arr.length; i++){
             lowerSum.add(sum-arr[i]);
         }
-        int high = arr.length;
+        int high = arr.length - 1;
         int low = 0;
         while(low <= high){
             if(lowerSum.contains(arr[low] + arr[high])) return true;
@@ -14,6 +20,25 @@ class Triplet{
             else{
                 high--;
             }
+        }
+        return false;
+    }
+    
+    //O(n^2) runtime, O(1) memory.
+    static boolean tripletSum2(int[] arr, int sum){
+        Arrays.sort(arr);
+        int curr = 0;
+        while(curr < arr.length){
+            int low = curr;
+            int high = arr.length-1;
+            while(low <= high){
+                if(arr[curr] + arr[low] + arr[high] == sum) return true;
+                else if(arr[curr] + arr[low] + arr[high] < sum) low++;
+                else{
+                    high--;
+                }
+            }
+            curr++;
         }
         return false;
     }
