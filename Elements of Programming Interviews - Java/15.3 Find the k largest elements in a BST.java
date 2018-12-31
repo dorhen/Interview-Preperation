f629775e9a345b15fca599592a82abbb069a28a8
dorhen <44751515+dorhen@users.noreply.github.com>
2018-12-31 16:00:33 +0200

    Update 15.3 Find the k largest elements in a BST.java

--- Elements of Programming Interviews - Java/15.3 Find the k largest elements in a BST.java
+++ Elements of Programming Interviews - Java/15.3 Find the k largest elements in a BST.java
@@ -1,14 +1,11 @@
-public static ArrayList<Integer> kLargest(BstNode root, int k){
-    ArrayList<Integer> toReturn = new ArrayList<Integer>();
-    Integer c = k;
-    findKLargest(root,toReturn,c);
-    return toReturn;
+ArrayList<Integer> kLargestBST(TreeNode<Integer> t, int k){
+    ArrayList<Integer> arr = new ArrayList<>();
+    kLargest(t,arr,k);
 }
 
-public static void findKLargest(BstNode root, ArrayList<Integer> lst, Integer c){
-    if(c == 0) return;
-    if(root.right != null) findKLargest(root.right,toReturn,c);
-    lst.add(root.data);
-    c--;
-    if(root.left != null) findKLargest(root.left,lst,c);
+void kLargest(TreeNode<Integer> root, ArrayList<Integer> arr, int k){
+    if(arr.size() == k) return;
+    if(root.right != null) kLargest(root.right,arr,k);
+    arr.add(root.data);
+    if(root.left != null) kLargest(root.left,arr,k);
 }
